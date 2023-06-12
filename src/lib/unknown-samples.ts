e6ab4c2607157169da9ebfa2d0356fa9ff601a94
Nathan Krasney <natankrasney@gmail.com>
2023-06-12 21:07:57 +0300

    ...

--- src/lib/unknown-samples.ts
+++ src/lib/unknown-samples.ts
@@ -1,11 +1,19 @@
 export function unknownSamples(): void {
   console.log("---------- unknownSamples");
 
-  let a : unknown = 1; // -- ok 
+  let a: unknown = 1; // -- ok
   console.log(`a : any is a number : ${a}`);
-  a = "hello world"; // -- ok 
-  a = 4; // -- ok 
-  //let num : number = a; --->  error !!!!!! you can not assign 
+  a = "hello world"; // -- ok
+  a = 4; // -- ok
+  //let num : number = a; --->  error !!!!!! you can not assign
+
+  canYouBeliveThisItsOk();
 }
 
+function canYouBeliveThisItsOk() {
+  let obj: unknown = "string";
+  // this issue no error or warning on development
 
+  // --> this give error because no this property in unknown
+  // obj.this.is.stupid.but.ok.with.ts(); 
+}
