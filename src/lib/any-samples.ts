7e72b22cdcde23aa2b25bd658268dc875f87e6bc
Nathan Krasney <natankrasney@gmail.com>
2023-06-12 22:41:02 +0300

    add better samples

--- src/lib/any-samples.ts
+++ src/lib/any-samples.ts
@@ -1,19 +1,25 @@
 export function anySamples(): void {
   console.log("---------- anySamples");
 
-  assign();
+  assignAnythingToType_any();
   addNumberSample();
   canYouBeliveThis();
+  // --- no compile problem , but this will be undefined on run time
+  logWithProblem(11); 
 }
 
-function assign() {
+function logWithProblem(array: any) {
+  console.log(`array.length : ${array.length}`);
+}
+
+function assignAnythingToType_any() {
   console.log("-- assign");
 
-  let a: any = 1;
+  let a: any = 1; // -- assign number to any
   console.log(`a : any is a number : ${a}`);
-  a = "hello world";
+  a = "hello world"; // -- assign string to any
   console.log(`a : any is a string : ${a}`);
-  a = (n1: number, n2: number) => n1 * n2;
+  a = (n1: number, n2: number) => n1 * n2; // -- assign function to any
   console.log(
     `a : any is a multiply function : ${a} , use a to multiply 2,4 :  ${a(
       2,
@@ -36,14 +42,13 @@ function addNumberSample() {
   console.log(`invalidResult = addNumbers("5", 10) : ${invalidResult}`);
 }
 
-function canYouBeliveThis(){
-  let obj : any = "string";
+function canYouBeliveThis() {
+  let obj: any = "string";
   // this issue no error or warning on development
   try {
     obj.this.is.stupid.but.ok.with.ts();
   } catch (error) {
     console.log(`catch obj.this.is.stupid.but.ok.with.ts()`);
-        
   }
 }
 
--- src/lib/unknown-samples.ts
+++ src/lib/unknown-samples.ts
@@ -1,13 +1,42 @@
 export function unknownSamples(): void {
   console.log("---------- unknownSamples");
 
+  youCanAssignAnythingToUnknown();
+  youCanAssignUnknownOnlyTo_any();
+
+  canYouBeliveThisItsOk();
+  logWithProblemFix([1,2,3]);
+  try {
+    logWithProblemFix("hello");  // throw
+  } catch (err) {
+    console.log(err);
+    
+  }
+  
+}
+
+function logWithProblemFix(array: unknown) {
+  if (Array.isArray(array)) {
+    console.log(`array.length : ${array.length}`);
+  }
+  else{
+    throw Error('no array')
+  }
+}
+
+function youCanAssignUnknownOnlyTo_any() {
+  const val: unknown = 11;
+  // const num : number = val; ---> error you can assign unknown to nothing beside any
+  // console.log(num);
+  const a: any = val; // --- pnly any can be assign to unknown
+  console.log(a);
+}
+
+function youCanAssignAnythingToUnknown() {
   let a: unknown = 1; // -- ok
   console.log(`a : any is a number : ${a}`);
   a = "hello world"; // -- ok
-  a = 4; // -- ok
-  //let num : number = a; --->  error !!!!!! you can not assign
-
-  canYouBeliveThisItsOk();
+  a = 4;
 }
 
 function canYouBeliveThisItsOk() {
@@ -15,5 +44,5 @@ function canYouBeliveThisItsOk() {
   // this issue no error or warning on development
 
   // --> this give error because no this property in unknown
-  // obj.this.is.stupid.but.ok.with.ts(); 
+  // obj.this.is.stupid.but.ok.with.ts();
 }
